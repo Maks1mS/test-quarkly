ae5d5e7202d0a77d0233f66d950bb677520984c0
Maxim Slipenko <36362599+Maks1mS@users.noreply.github.com>
2020-09-28 16:18:07 +0300

    Some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,11 @@ export default (() => {
 	return <Theme theme={theme}>
 		<Components.BackToTop>
 			<Section height="400vh" background="--color-greyD2">
-				<Components.BackToTop>
-					<Box background="#588e8e" height="200vh" />
-				</Components.BackToTop>
+				<Box background="#588e8e" height="150vh">
+					<Components.BackToTop>
+						<Box background="#8e587c" height="200vh" />
+					</Components.BackToTop>
+				</Box>
 			</Section>
 		</Components.BackToTop>
 	</Theme>;
