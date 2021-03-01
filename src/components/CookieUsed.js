e489061c1af7fbee569290cc157c4fcb7ac3a38d
Maxim Slipenko <36362599+Maks1mS@users.noreply.github.com>
2021-03-01 09:18:28 +0300

    Changes

--- src/components/CookieUsed.js
+++ src/components/CookieUsed.js
@@ -3,7 +3,7 @@ import atomize from '@quarkly/atomize';
 import { Box, Button, Text } from '@quarkly/widgets';
 const item = 'accept_cookies';
 
-const getAPI = () => (window || global)?.QAPI || {};
+const getAPI = () => (global || window)?.QAPI || {};
 
 const CookieUsed = props => {
 	const [show, setShow] = useState(false);
@@ -11,22 +11,35 @@ const CookieUsed = props => {
 
 	const handleClick = () => {
 		localStorage.setItem(item, true);
+		if (getAPI().mode === 'development') return;
 		setShow(false);
 	};
 
 	useEffect(() => {
 		const acceptCookies = localStorage.getItem(item) || false;
+
+		if (getAPI().mode === 'development') {
+			setShow(true);
+			return;
+		}
+
 		setShow(!acceptCookies);
 	}, []);
-	return <Box display={show ? 'block' : 'none'} background="--color-primary" {...props}>
+	return <Box
+		display={show ? 'block' : 'none'}
+		background="--color-primary"
+		text-align="center"
+		padding="10px"
+		{...props}
+	>
 		    
-		<Text>
+		<Text color="--color-light">
 			We use cookies to improve your experience on our site.
     By using our site, you consent to out use of cookies.
 		</Text>
 		    
-		<Button onClick={handleClick}>
-			Agree
+		<Button color="--color-primary" background="--color-light" onClick={handleClick}>
+			Accept
 		</Button>
 		  
 	</Box>;
